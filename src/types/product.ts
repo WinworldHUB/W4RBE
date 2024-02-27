65b40ff1c38fb11aebce8b0d4c4b2aa7f768cd97
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-28 01:00:17 +0530

    add products api and es6 syntax

---
+++ src/types/product.ts
@@ -0,0 +1,19 @@
+export type Product = {
+    id: string;
+    title: string;
+    body: string;
+    category: string;
+    published: boolean;
+    size: string;
+    variants: {
+      size: string;
+      available: boolean;
+      price: number;
+      quantity: number;
+    }[];
+    quantity: number;
+    price: number;
+    taxable: boolean;
+    featuredImage: string;
+    otherImages: string;
+  };
