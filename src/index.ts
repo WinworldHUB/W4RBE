a5953d14d62774ee51a2a99cb334bd3d232d1b85
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-04 21:03:30 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
+import { json } from "body-parser";
+import cors from "cors"
 import productRoutes from "./routes/products-routes";
 import memberRoutes from "./routes/members-routes";
-import { json } from "body-parser";
 import orderRoutes from "./routes/orders-routes";
-
 const app = express();
 const PORT = 3000;
 
 // app.use(json());
 app.use(express.json({ limit: "10mb" }));
-
+app.use(cors({origin:"http://localhost:3001"}))
 app.use("/products", productRoutes);
 app.use("/members", memberRoutes);
 app.use("/orders",orderRoutes)
