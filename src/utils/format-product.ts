15582f0dd9b619bcfb10b39b2cd480a0924c74e2
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-05 23:40:37 +0530

    Fix the format of products for import products api

---
+++ src/utils/format-product.ts
@@ -0,0 +1,75 @@
+import { Product } from "../types/product";
+
+export function formatProducts(productsData:Product[]) {
+    const formattedProducts = {};
+
+    const processedProductIds = new Set();
+
+    productsData.forEach((product:Product) => {
+        const id = product["Handle"];
+        if (processedProductIds.has(id)) {
+            return;
+        }
+        processedProductIds.add(id);
+
+        const title = product["Title"];
+        const bodyHtml = product["Body (HTML)"];
+
+        console.log("Product:", id, title, bodyHtml);
+
+        if (bodyHtml === undefined) {
+            return; 
+        }
+
+        let category;
+        if (id.toLowerCase().includes("bulk")) {
+            category = "Bulk";
+        } else {
+            category = product["Product Category"];
+        }
+        const published = product["Published"];
+        const price = product["Variant Price"];
+        const taxable = product["Variant Taxable"];
+        const featuredImage = product["Image Src"];
+        const otherImages = product["Image Alt Text"];
+        const size = product["Option1 Value"];
+        const variantPrice = product["Variant Price"];
+        const variantQuantity = product["Variant Inventory Qty"];
+        const bodyText = stripHtml(bodyHtml);
+
+        if (formattedProducts[id]) {
+            formattedProducts[id].variants.push({
+                size: size,
+                available: true,
+                price: variantPrice,
+                quantity: variantQuantity
+            });
+        } else {
+            formattedProducts[id] = {
+                id: id,
+                title: title,
+                body: bodyText,
+                category: category,
+                published: published,
+                size: size,
+                variants: [{
+                    size: size,
+                    available: true,
+                    price: variantPrice,
+                    quantity: variantQuantity
+                }],
+                price: price,
+                taxable: taxable,
+                featuredImage: featuredImage,
+                otherImages: otherImages
+            };
+        }
+    });
+
+    return Object.values(formattedProducts);
+}
+
+function stripHtml(html:string) {
+    if (!html) return ''; 
+    return html.replace(/<[^>]*>?/gm, '');
+}
