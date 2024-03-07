3e9474519f829a86a464510adc919d2f06ad5608
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2024-03-07 20:38:53 +0530

    W4 rbe 07 fix members api (#9)
    * fetch users by email
    
    * add amplify auth
    
    * fix api endpoints

--- src/utils/validate-user.ts
+++ src/utils/validate-user.ts
@@ -1,7 +1,20 @@
 import { Member } from "../types/member";
 import { getMember, insertMember, updateMember } from "../models/members-model";
 import _ from "lodash";
+import {
+  AuthUser,
+  SignUpInput,
+  fetchAuthSession,
+  getCurrentUser,
+  signIn,
+  signOut,
+  signUp,
+} from "aws-amplify/auth";
+import config from "../amplifyconfiguration.json";
+import { Amplify } from "aws-amplify";
 
+
+Amplify.configure(config);
 const validateMemberData = (
   jsonData: Member[]
 ): {
@@ -37,7 +50,23 @@ const validateMemberData = (
           response.updatedUsers.push(newUser);
         }
       } else {
-        insertMember(newUser);
+        const insertedMember = insertMember(newUser);
+        if (insertedMember) {
+          const signUpDetails: SignUpInput = {
+            username: insertedMember["Customer email"],
+            password: "Password@1",
+            options: {
+              autoSignIn: true,
+              userAttributes: {
+                email: insertedMember["Customer email"],
+                name: insertedMember["Customer name"],
+              },
+            },
+          };
+          signUp(signUpDetails)
+            .then((value) => console.log(value))
+            .catch((reason) => console.error(reason));
+        }
         response.newUsers.push(newUser);
       }
     } else {
