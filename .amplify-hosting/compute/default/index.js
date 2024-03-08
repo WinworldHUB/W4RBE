fead75c99f27a27c4c2b4deb8280305f3dc71e98
Amit Dubey <adubey@cynergybank.co.uk>
2024-03-08 11:53:48 +0000

    updated

--- .amplify-hosting/compute/default/index.js
+++ .amplify-hosting/compute/default/index.js
@@ -5,10 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const products_routes_1 = __importDefault(require("./routes/products-routes"));
+const members_routes_1 = __importDefault(require("./routes/members-routes"));
+const orders_routes_1 = __importDefault(require("./routes/orders-routes"));
 const app = (0, express_1.default)();
 const PORT = 3000;
-app.use(express_1.default.json());
+app.use(express_1.default.json({ limit: "10mb" }));
+app.use((req, res, next) => {
+    const allowedOrigins = [
+        "https://main.d3a3bx78s8tswk.amplifyapp.com",
+        "http://localhost:3001",
+        "http://localhost:3000",
+    ];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    return next();
+});
 app.use("/products", products_routes_1.default);
+app.use("/members", members_routes_1.default);
+app.use("/orders", orders_routes_1.default);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "w4r8668d96a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
