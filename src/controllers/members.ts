d7140c0acd653b8b7e59861d7ec2ad78a35d032a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-12 22:52:09 +0530

    implement addMember controller using graphql

--- src/controllers/members.ts
+++ src/controllers/members.ts
@@ -1,6 +1,9 @@
 import { generateClient } from "aws-amplify/api";
 import { listMembers, getMember } from "../graphql/queries";
 import { Amplify, ResourcesConfig } from "aws-amplify";
+import { RequestHandler } from "express";
+import { Member } from "../awsApis";
+import { createMember } from "../graphql/mutations";
 //import config from "../aws-exports.js";
 
 const config: ResourcesConfig = {
@@ -19,7 +22,7 @@ Amplify.configure(config);
 
 const client = generateClient();
 
-export const getAllMembers = async (req, res, next) => {
+export const getAllMembers:RequestHandler = async (req, res, next) => {
   try {
     const members = await client.graphql({
       query: listMembers,
@@ -32,3 +35,29 @@ export const getAllMembers = async (req, res, next) => {
     res.status(500).json({ error: "Failed to retrieve members" });
   }
 };
+
+
+export const addMember: RequestHandler = async (req, res, next) => {
+  try {
+    const member = req.body as Member;
+
+    if (member) {
+      const newMember = await client.graphql({
+        query: createMember,
+        variables: {
+          input: member,
+        },
+      });
+
+      res.json(newMember.data.createMember);
+    }
+  } catch (error) {
+    return res.status(500).send({
+      status: "failed",
+      message: "Error saving member",
+      internalError: error,
+    });
+  }
+};
+
+
--- src/routes/members-routes.ts
+++ src/routes/members-routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getAllMembers } from "../controllers/members";
+import { addMember, getAllMembers } from "../controllers/members";
 
 const memberRoutes = Router();
 memberRoutes.get("/", getAllMembers);
 // memberRoutes.get("/:email", getMemberById);
-// memberRoutes.post("/", addMember);
+memberRoutes.post("/", addMember);
 // memberRoutes.post("/imports", importMembers);
 // memberRoutes.put("/:id", modifyMember);
 // memberRoutes.delete("/:id", deleteMemberById);
