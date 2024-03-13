6cff08df0bb4c6b6cacfb5f1e5415430760a9fae
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2024-03-13 14:44:13 +0530

    W4 rbe 10 add member graph ql api (#12)
    * implement addMember controller using graphql
    
    * add get member by id graphql implementation
    
    * add delete member grapgql implementation
    
    * implement update member mutation
    
    * add formatter for import members
    
    * update logic for getting user by email
    
    * add validations and update members formatter
    
    * rename file

---
+++ src/utils/format-user.ts
@@ -0,0 +1,26 @@
+import { Member } from "../awsApis";
+
+function formatMemberData(data): Member[] {
+  const formattedMembers = [];
+
+  data.forEach((item) => {
+    const formattedMember = {
+      id: item.ID.toString(),
+      name: item["Customer name"],
+      email: item["Customer email"],
+      phone: item["Customer phone"] !== "" ? item["Customer phone"] : null,
+      active: item.Status === "Active",
+      province: item["Delivery province code"] !== "" ? item["Delivery province code"] : null,
+      city: item["Delivery city"] !== "" ? item["Delivery city"] : null,
+      zip: item["Delivery zip"] !== "" ? item["Delivery zip"] : null,
+      country: item["Delivery country code"] !== "" ? item["Delivery country code"] : null,
+      address1: item["Delivery address 1"] !== "" ? item["Delivery address 1"] : null,
+      address2: item["Delivery address 2"] !== "" ? item["Delivery address 2"] : null,
+    };
+    
+    formattedMembers.push(formattedMember);
+  });
+  return formattedMembers;
+}
+
+export default formatMemberData;
