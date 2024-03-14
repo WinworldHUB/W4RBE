d31ccb372e76a0dbcfa03af1452a502e5ef9e6b2
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2024-03-14 19:51:50 +0530

    add allow-origins header n cors (#15)

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = 3000;
 app.use(express.json({ limit: "10mb" }));
 app.use((req, res, next) => {
   const allowedOrigins = [
-    "https://main.dt2z8g98byon5.amplifyapp.com/",
+    "https://main.dt2z8g98byon5.amplifyapp.com",
     "http://localhost:3001",
     "http://localhost:3000",
   ];
@@ -18,10 +18,11 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers");
   res.header("Access-Control-Allow-Credentials", "true");
   return next();
 });
+
 
 app.use("/products", productRoutes);
 app.use("/members", memberRoutes);
