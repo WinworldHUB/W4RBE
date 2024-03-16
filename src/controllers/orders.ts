ea4dbb2323d58246802b29e64e5a11c5633dc039
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-16 21:32:22 +0530

    add config

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,7 +1,26 @@
 import { RequestHandler } from "express";
+import { generateClient } from "aws-amplify/api";
+import { listOrders } from "../graphql/queries";
+import { Amplify, ResourcesConfig } from "aws-amplify";
 import { Order } from "../types/orders";
+const config: ResourcesConfig = {
+  API: {
+    GraphQL: {
+      endpoint:
+        "https://srcgirnqdvfpvpaktygytjn2pe.appsync-api.eu-west-2.amazonaws.com/graphql",
+      region: "eu-west-2",
+      defaultAuthMode: "apiKey",
+      apiKey: "da2-tsfh46xxpzgcbfldx6qkees5we",
+    },
+  },
+};
 
-export const getOrderById: RequestHandler = (req, res, next) => res.json();
+Amplify.configure(config);
+
+const client = generateClient();
+export const getOrderById: RequestHandler = (req, res, next) => {
+  
+};
 
 export const getAllOrders: RequestHandler = (req, res, next) => res.json();
 
