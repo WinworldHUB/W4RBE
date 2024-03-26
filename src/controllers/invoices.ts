b0801afad02767b48d8825d2dc55997246c07938
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-26 21:15:54 +0530

    add update invoice api

--- src/controllers/invoices.ts
+++ src/controllers/invoices.ts
@@ -3,6 +3,8 @@ import { Amplify } from "aws-amplify";
 import { AWS_API_CONFIG } from "../constants/constants";
 import { generateClient } from "aws-amplify/api";
 import { getInvoice, listInvoices } from "../graphql/queries";
+
+import { updateInvoice } from "../graphql/mutations";
 import { Invoice } from "../awsApis";
 import jwt from "jsonwebtoken";
 
@@ -17,8 +19,6 @@ export const getInvoiceById: RequestHandler = async (req, res, next) => {
         id: req.params.id,
       },
     });
-
-    console.log(invoice);
     res.json(invoice.data.getInvoice);
   } catch (error) {
     console.log(error);
@@ -41,3 +41,32 @@ export const getAllInvoices: RequestHandler = async (req, res, next) => {
         res.status(500).json({message: "Failed to retrieve invoices", error: error});
     }
 };
+
+
+export const modifyInvoice: RequestHandler = async (req, res, next) => {
+    try {
+        const invoice = req.body as Invoice;
+        if (!invoice) {
+            res.status(500).json({
+              message:
+                "Invalid order. Order details are not in correct format or empty",
+              error: null,
+            });
+            return;
+          }
+      
+        const updatedInvoice = await client.graphql({
+            query: updateInvoice,
+            variables: {
+                input: {
+                    id: invoice.id,
+                }
+            }
+        })
+        res.json(updatedInvoice.data.updateInvoice);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to update invoice", error: error });
+    }
+}
--- src/routes/invoices-routes.ts
+++ src/routes/invoices-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { getInvoiceById, getAllInvoices } from "../controllers/invoices";
+import { getInvoiceById, getAllInvoices, modifyInvoice } from "../controllers/invoices";
 
 const invoiceRoutes = Router();
 
 invoiceRoutes.get("/", getAllInvoices);
 invoiceRoutes.get("/:id", getInvoiceById);
+invoiceRoutes.put("/:id", modifyInvoice);
 
 export default invoiceRoutes;
