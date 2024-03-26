c2ab7260c515864104f3bf00cbcedca38ba34b1a
Amit Dubey <adubey@cynergybank.co.uk>
2024-03-26 13:11:15 +0000

    Updated

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   const allowedOrigins = [
     "https://main.dt2z8g98byon5.amplifyapp.com",
     "https://master.d212wzb60r31lk.amplifyapp.com",
+    "https://bulk.wholesale4resale.com",
+    "https://bulkadmin.wholesale4resale.com",
     "http://localhost:3001",
     "http://localhost:3000",
   ];
@@ -18,8 +20,14 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With, Access-Control-Allow-Origin, Access-Control-Allow-Headers");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, Content-Type, X-Requested-With, Access-Control-Allow-Origin, Access-Control-Allow-Headers"
+  );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
