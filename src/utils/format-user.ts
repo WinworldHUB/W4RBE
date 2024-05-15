76942a2f4957edaf340c5d19f5c8cf95b63bda0b
Amit Dubey <iamsaagar@yahoo.com>
2024-05-15 16:55:24 +0100

    Updated

--- src/utils/format-user.ts
+++ src/utils/format-user.ts
@@ -3,30 +3,43 @@ import { Member } from "../awsApis";
 function formatMemberData(members): Member[] {
   const formattedMembers = [];
 
- members.forEach((member) => {
+  members.forEach((member) => {
     try {
-
-      const formattedMember = {
-        id: member.ID.toString(),
-        name: member["Customer name"],
-        email: member["Customer email"],
-        phone: member["Customer phone"] !== "" ? member["Customer phone"] : null,
-        active: true,
-        province: member["Delivery province code"] !== "" ? member["Delivery province code"] : null,
-        city: member["Delivery city"] !== "" ? member["Delivery city"] : null,
-        zip: member["Delivery zip"] !== "" ? member["Delivery zip"] : null,
-        country: member["Delivery country code"] !== "" ? member["Delivery country code"] : null,
-        address1: member["Delivery address 1"] !== "" ? member["Delivery address 1"] : null,
-        address2: member["Delivery address 2"] !== "" ? member["Delivery address 2"] : null,
-        deliveryPerson: "",
-        deliveryEmail: "",
-        deliveryAddress1: "",
-        deliveryAddress2: ""
-      } as Member; 
-      
+      const formattedMember = member["id"]
+        ? member
+        : ({
+            id: member.ID.toString(),
+            name: member["Customer name"],
+            email: member["Customer email"],
+            phone:
+              member["Customer phone"] !== "" ? member["Customer phone"] : null,
+            active: true,
+            province:
+              member["Delivery province code"] !== ""
+                ? member["Delivery province code"]
+                : null,
+            city:
+              member["Delivery city"] !== "" ? member["Delivery city"] : null,
+            zip: member["Delivery zip"] !== "" ? member["Delivery zip"] : null,
+            country:
+              member["Delivery country code"] !== ""
+                ? member["Delivery country code"]
+                : null,
+            address1:
+              member["Delivery address 1"] !== ""
+                ? member["Delivery address 1"]
+                : null,
+            address2:
+              member["Delivery address 2"] !== ""
+                ? member["Delivery address 2"]
+                : null,
+            deliveryPerson: "",
+            deliveryEmail: "",
+            deliveryAddress1: "",
+            deliveryAddress2: "",
+          } as Member);
       formattedMembers.push(formattedMember);
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Error formatting member data: ", error);
     }
   });
